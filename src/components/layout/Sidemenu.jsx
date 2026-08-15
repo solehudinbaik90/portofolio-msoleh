f558e710b670f8464e2993b85a106514cd3afe60
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-15 19:22:23 +0700

    Update footer link text from 'KFL' to 'Msoleh'

--- src/components/layout/Sidemenu.jsx
+++ src/components/layout/Sidemenu.jsx
@@ -43,7 +43,7 @@ const Sidemenu = ({ fullMenu, fullMenuHandler }) => {
           </span>
           <Link href="/">
             <a className="block font-medium text-heading no-underline hover:text-primary lg:inline">
-              KFL
+              Msoleh
             </a>
           </Link>
         </p>
