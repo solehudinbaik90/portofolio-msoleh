a879dc137b2d315c7d6f76a8aba327b92dcacb6b
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-15 18:30:10 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        deviceSizes: [640, 768, 1024, 1280, 1536],
+        loader: "custom",
+        path: "/",
+    },
+    basePath: "",
+    assetPrefix: ""
+};
+
+// module.exports = nextConfig;
+module.exports = withPlugins(
+    [
+        [
+            withPWA,
+            {
+                pwa: {
+                    disable: process.env.NODE_ENV === "development",
+                    dest: "public",
+                    runtimeCaching,
+                },
+            },
+        ],
+    ],
+    nextConfig
+);
