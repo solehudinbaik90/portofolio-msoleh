e005bb4ccb987ab92ed9a5a36dc94e1b266d5277
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-16 12:46:20 +0700

    Add GoogleMap to exports in index.js

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -3,5 +3,6 @@ import PortfolioFilters from "./PortfolioFilters";
 import SectionHeading from "./SectionHeading";
 import Spinner from "./Spinner";
 import Comments from "./Comments";
+import GoogleMap from "./GoogleMap";
 
-export { Logo, SectionHeading, PortfolioFilters, Spinner, Comments };
+export { Logo, SectionHeading, PortfolioFilters, Spinner, Comments, GoogleMap };
