e7ec4de1165cbbbc262fac0a15a27832415fb99f
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-18 19:09:35 +0700

    Update emailjs_publickey in settings.js

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,5 +1,5 @@
 export const settings = {    
     emailjs_serviceid: "service_8y9lroj",
     emailjs_templateid: "template_e1yoitq",
-    emailjs_publickey: "54fewO_YH09gubUt",
+    emailjs_publickey: "54fewO_YH09gubUt-",
 };
